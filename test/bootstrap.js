ec43de9a083bd829e5eaa3b32ae9fbfa319d6555
thecodrr <thecodrr@protonmail.com>
2022-07-20 07:06:29 +0500

    test: use jsdom during tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,6 +1,8 @@
 //.webstorm.bootstrap.js
 var chai = require('chai');
+var jsdom = require('jsdom');
 global.chai = chai;
 global.expect = chai.expect;
 global.showdown = require('../.build/showdown.js');
 global.getDefaultOpts = require('./optionswp.js').getDefaultOpts;
+global.showdown.helper.document = new jsdom.JSDOM('', {}).window.document;
