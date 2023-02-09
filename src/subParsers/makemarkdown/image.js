7c083cddf42cdc7aedd2bfc25ff1e492d44fca0f
Abdullah Atta <abdullahatta@streetwriters.co>
2023-02-09 13:08:45 +0500

    fix: do not add width/height in md images

--- src/subParsers/makemarkdown/image.js
+++ src/subParsers/makemarkdown/image.js
@@ -3,16 +3,11 @@ showdown.subParser('makeMarkdown.image', function (node) {
 
   var txt = '';
   if (node.hasAttribute('src') && node.getAttribute('src') !== '') {
-    txt += '![' + node.getAttribute('alt') + '](';
+    txt += '![' + (node.getAttribute('alt') || '') + '](';
     txt += '<' + node.getAttribute('src') + '>';
-    if (node.hasAttribute('width') && node.hasAttribute('height')) {
-      var width = node.getAttribute('width');
-      var height = node.getAttribute('height');
-      txt += ' =' + (width === 'auto' ? '*' : width) + 'x' + (height === 'auto' ? '*' : height);
-    }
 
     if (node.hasAttribute('title')) {
-      txt += ' "' + node.getAttribute('title') + '"';
+      txt += ' "' + (node.getAttribute('title') || '') + '"';
     }
     txt += ')';
   }
